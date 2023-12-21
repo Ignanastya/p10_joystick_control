598b82dd87f822100dfe6260e5f3bbea092f02b7
Ignanastya <115129447+Ignanastya@users.noreply.github.com>
2023-12-21 19:59:00 +0700

    Add files via upload

---
+++ MDK-ARM/Lab1_v2/p10.d
@@ -0,0 +1,4 @@
+lab1_v2\p10.o: ..\..\Lab_1\Core\Src\P10.c
+lab1_v2\p10.o: ..\..\Lab_1\Core\Src\../inc/P10.h
+lab1_v2\p10.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+lab1_v2\p10.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ MDK-ARM/Lab1_v2/startup_stm32f411xe.d
@@ -0,0 +1,1 @@
+lab1_v2\startup_stm32f411xe.o: startup_stm32f411xe.s
